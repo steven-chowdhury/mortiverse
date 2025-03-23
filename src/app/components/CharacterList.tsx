8fbdec2042c7a3db3af662b27f908d1389a45162
Steven Chowdhury <steven.chowdhury@gmail.com>
2025-03-23 15:41:37 -0700

    Load list of characters from graphql api, create Character type

--- src/app/components/CharacterList.tsx
+++ src/app/components/CharacterList.tsx
@@ -1,19 +1,70 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache()
+})
 
 const CharacterList = () => {
+  const [ characters, setCharacters ] = useState<Character[]>([])
+
+  useEffect(() => {
+    async function getResults() {
+      const result = await client.query({
+        query: gql`
+          query {
+            characters {
+              results {
+                id
+                name
+                image
+                species
+                status
+                location {
+                  name
+                }
+              }
+            }
+          }
+        `
+      })
+
+      const characters = result.data.characters.results
+
+      setCharacters(characters)
+
+      console.log('result =>', result)
+    }
+
+    getResults()
+    
+  }, [])
+
   return (
     <ul>
-      <li>
-        <h2>Rick Sanchez</h2>
-        <Image 
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt={'Rick Sanchez'}
-          width={100}
-          height={100}
-        />
-        <div>Locaion: Citadel of Ricks</div>
-      </li>
+      { characters.map(character => (
+        <li style={{ display: 'flex', gap: '20px' }} key={character.id}>
+          <Image 
+            src={character.image}
+            alt={character.name}
+            width={100}
+            height={100}
+          />
+          <div>
+            <h2>{character.name}</h2>
+            <div>Status: {character.status}</div>
+            <div>Species: {character.species}</div>
+            <div>Location: {character.location.name}</div>
+          </div>
+        </li>
+      ))}
+      
     </ul>
   )
 }
